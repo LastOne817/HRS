49393d705808e08c5c87b39b8333df4ac3050d3e
LastOne817 <lastone817@naver.com>
2015-04-26 16:49:46 +0900

    Testcode for signup, login, logout
    Actual progress not tested yet

--- test/controllers/sessions_controller_test.rb
+++ test/controllers/sessions_controller_test.rb
@@ -1,7 +1,19 @@
 require 'test_helper'
 
 class SessionsControllerTest < ActionController::TestCase
-  # test "the truth" do
-  #   assert true
-  # end
+    test '#signup_trial' do
+        get :new
+        assert_redirected_to '/auth/facebook?type=signin'
+    end
+
+    test '#login_trial' do
+        get :find
+        assert_redirected_to '/auth/facebook?type=login'
+    end
+
+    test '#logout' do
+        get :destroy
+        assert session[:user_id] == nil
+        assert_redirected_to root_path
+    end
 end
