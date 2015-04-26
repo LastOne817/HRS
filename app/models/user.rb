483c87c8840bd03b53739287f076856ada8e82d5
LastOne817 <lastone817@naver.com>
2015-04-26 11:12:24 +0900

    Fixed facebook signup bug

--- app/models/user.rb
+++ app/models/user.rb
@@ -2,15 +2,15 @@ class User < ActiveRecord::Base
     validates :password, length: { minimun: 5, maximum: 20 }
     validates :email, uniqueness: true
     def self.omniauth(auth)
-        create! do |user|
-            user.provider = auth.provider
-            user.uid = auth.uid
-            if auth.info
-                user.username = auth.info.name
-                user.email = auth.info.email
-                user.image = auth.info.image
-                user.password = auth.uid
-            end
+        @user = User.new
+        @user.provider = auth.provider
+        @user.uid = auth.uid
+        if auth.info
+            @user.username = auth.info.name
+            @user.email = auth.info.email
+            @user.image = auth.info.image
+            @user.password = auth.uid
         end
+        return @user
     end
 end
