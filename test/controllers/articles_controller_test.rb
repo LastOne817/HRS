ad704c66b1c75e41390eba9e3e5481a265d4d050
HurSungYeon <ethan0311@naver.com>
2015-05-10 21:30:43 +0900

    articles test

--- test/controllers/articles_controller_test.rb
+++ test/controllers/articles_controller_test.rb
@@ -1,7 +1,12 @@
 require 'test_helper'
 
 class ArticlesControllerTest < ActionController::TestCase
-  # test "the truth" do
-  #   assert true
-  # end
+
+  test '#article_create_success' do
+      request.env["HTTP_REFERER"] = "/pages/checklist"
+      post :create, {'first' => 1, 'second' => 2, 'third' => 3, 'fourth' => 1 }
+      assert_redirected_to "/articles/1"
+  end
+
+  
 end
