0655de5766f4e5f469fb637dbdf1aa89e8694bf5
HurSungYeon <ethan0311@naver.com>
2015-05-20 14:20:37 +0900

    add 3 steps

--- features/signup.feature
+++ features/signup.feature
@@ -3,7 +3,7 @@ Feature: User sign up
 Scenario: User sign up success
     Given the main page of the site
     When I click "Sign up" message button
-    Then “signup_page” should be opened.
+    Then "Sign up" page should be opened
     When I fill E-mail, Nickname, Password, Password confirm
     And I press “submit” button
     Then the main page should be seen and I should see welcoming message
--- features/step_definitions/signup_steps.rb
+++ features/step_definitions/signup_steps.rb
@@ -2,11 +2,15 @@ Given /^the main page of the site$/ do
     visit('/')
 end
 
-When /^I click "(\w+)" message button$/ do |button|
-    click_link(button)
-end
-
 When(/^I click "(.*?)" message button$/) do |arg1|
     click_link(arg1)
+end
+
+Then(/^"(.*?)" page should be opened$/) do |arg1|
+  assert page.has_content?("Sign up with Facebook")
+end
+
+When(/^I fill E\-mail, Nickname, Password, Password confirm$/) do
+  
 end
 
