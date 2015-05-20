36598d3f3f44ae1889e40c2fe888ce39fcd52b30
HurSungYeon <ethan0311@naver.com>
2015-05-20 14:26:35 +0900

    4th step

--- features/step_definitions/signup_steps.rb
+++ features/step_definitions/signup_steps.rb
@@ -7,10 +7,14 @@ When(/^I click "(.*?)" message button$/) do |arg1|
 end
 
 Then(/^"(.*?)" page should be opened$/) do |arg1|
-  assert page.has_content?("Sign up with Facebook")
+    assert page.has_content?("Sign up with Facebook")
 end
 
 When(/^I fill E\-mail, Nickname, Password, Password confirm$/) do
-  
+    fill_in('signup_email', :with => "test@bdd.cucumber")
+    fill_in('signup_username', :with => "behavior")
+    fill_in('signup_password', :with => "cucumberbdd")
+    fill_in('signup_password_confirmation', :with => "cucumberbdd")
+    click_button('signup_submit_button') 
 end
 
