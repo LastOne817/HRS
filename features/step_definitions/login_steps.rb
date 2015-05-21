46fd8fb9562eebdee5f26df318dd96a214e3dfbb
HurSungYeon <ethan0311@naver.com>
2015-05-21 17:49:26 +0900

    login step refactored

--- features/step_definitions/login_steps.rb
+++ features/step_definitions/login_steps.rb
@@ -17,17 +17,15 @@ end
 Given(/^I logged in as "(.*?)"$/) do |email|
 
 steps %Q{
+
      Given the main page of the site
      And I signed up as "#{email}" and "thisisBDD" and "cucumberbdd"
      When I click "Log in" message button
      Then I "login" page should be opened
      When I fill E-mail and password as "#{email}" and "cucumberbdd" in login page
      And I press "login_submit_button" button
-     Then the main page should be seen and I should see welcoming message
+     Then the main page should be seen
+     And I should see the message " Logged in Successfully"
   }
-end
-
-Then(/^the main page should be seen and I should see log out message$/) do
-  assert page.has_content?("logged out successfully")
 end
 
