ae9e8353fb905d949c8002c423246af55e468b42
HurSungYeon <ethan0311@naver.com>
2015-05-23 21:35:34 +0900

    hotfix for small typo

--- db/migrate/20150523113109_create_properties.rb
+++ db/migrate/20150523113109_create_properties.rb
@@ -1,7 +1,7 @@
 class CreateProperties < ActiveRecord::Migration
   def change
     create_table :properties do |t|
-      t.string name
+      t.string :name
       t.timestamps null: false
     end
   end
