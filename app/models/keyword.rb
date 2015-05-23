f30bab2228cdc6a5fab3f6a89ac7fcb0025a5c75
HurSungYeon <ethan0311@naver.com>
2015-05-23 20:44:58 +0900

    set relationship between keyword and property

--- app/models/keyword.rb
+++ app/models/keyword.rb
@@ -1,2 +1,5 @@
 class Keyword < ActiveRecord::Base
+    belongs_to :Property
+
+    validates :name, presence: true
 end
--- app/models/property.rb
+++ app/models/property.rb
@@ -1,2 +1,5 @@
 class Property < ActiveRecord::Base
+    has_many :Keywords
+
+    validates :name, presence: true
 end
