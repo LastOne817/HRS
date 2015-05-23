58f7f85ff2fe7af0864840e23985dfe8f2a5940f
HurSungYeon <ethan0311@naver.com>
2015-05-23 21:38:22 +0900

    fixed blamed one and add references in keyword model

--- db/migrate/20150523113529_create_keywords.rb
+++ db/migrate/20150523113529_create_keywords.rb
@@ -1,9 +1,11 @@
 class CreateKeywords < ActiveRecord::Migration
   def change
     create_table :keywords do |t|
-
+      t.references :property, index: true 
+ 
       t.string :name
       t.timestamps null: false
     end
+    add_foreign_key :keywords, :properties
   end
 end
