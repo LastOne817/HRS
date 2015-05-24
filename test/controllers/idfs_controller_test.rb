46bcc6a848b02bb95d0be7a8fa0749513cf25bfa
HurSungYeon <ethan0311@naver.com>
2015-05-24 19:57:03 +0900

    delete @'s 2

--- test/controllers/idfs_controller_test.rb
+++ test/controllers/idfs_controller_test.rb
@@ -3,15 +3,15 @@ class IdfsControllerTest < ActionController::TestCase
     test '#idf_createAll' do
         request.env["HTTP_REFERER"] = "/pages/main"
         post :createAll
-        @t = Idf.find_by(:prop => "samplesample")
-        assert @t != nil
+        t = Idf.find_by(:prop => "samplesample")
+        assert t != nil
     end
 
     test '#idf_update' do
         request.env["HTTP_REFERER"] = "/pages/main"
         post :update
-        @t = Idf.find_by(:prop => "sample")
-        assert (@t.value - Math.log(2.5)).abs <= 0.01
+        t = Idf.find_by(:prop => "sample")
+        assert (t.value - Math.log(2.5)).abs <= 0.01
     end
 
 
@@ -19,7 +19,7 @@ class IdfsControllerTest < ActionController::TestCase
         request.env["HTTP_REFERER"] = "/pages/main"
         post :createAll
         post :destroy, {"prop" => "sample"}
-        @t = Idf.find_by(:prop => "sample")
-        assert @t == nil
+        t = Idf.find_by(:prop => "sample")
+        assert t == nil
     end
 end
