155e419f03e9ec836ce4e954c7368fd75182ea27
HurSungYeon <ethan0311@naver.com>
2015-05-24 18:56:40 +0900

    change idf model

--- app/models/idf.rb
+++ app/models/idf.rb
@@ -1,4 +1,4 @@
 class Idf < ActiveRecord::Base
-    validates :value, :numericality => { :greater_than_or_equal_to => 1 }
+    #validates :value, :numericality => { :greater_than_or_equal_to => 1 }
     validates :prop, presence: true, uniqueness: true
 end
