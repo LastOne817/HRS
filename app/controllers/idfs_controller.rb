88e27d97345cda03ba8de21bbb6b339f0fc5d39d
HurSungYeon <ethan0311@naver.com>
2015-05-24 19:53:29 +0900

    delete @'s

--- app/controllers/idfs_controller.rb
+++ app/controllers/idfs_controller.rb
@@ -1,38 +1,38 @@
 class IdfsController < ApplicationController
       
     def createAll
-        @props = Property.all
+        props = Property.all
         
-        @props.each do |prop|
-            @t = Idf.new(:prop => prop.name, :value => 1.00)
-            @t.save
+        props.each do |prop|
+            t = Idf.new(:prop => prop.name, :value => 1.00)
+            t.save
         end
 
         redirect_to :back  #this is for temporary thing
     end
 
     def update
-        @idfs = Idf.all
+        idfs = Idf.all
 
-        @idfs.each do |idf|
+        idfs.each do |idf|
 
-            @prop = idf.prop
-            @max = 0
-            @contain = 0
-            @hobbies = Hobby.all
+            prop = idf.prop
+            max = 0
+            contain = 0
+            hobbies = Hobby.all
 
-            @hobbies.each do |hobby|
+            hobbies.each do |hobby|
 
-                @max = @max + 1
+                max = max + 1
    
-                @h = hobby.tfs.find_by(prop: @prop)
-                if @h !=nil and @h.value != 1
-                    @contain = @contain + 1
+                h = hobby.tfs.find_by(prop: prop)
+                if h !=nil and h.value != 1
+                    contain = contain + 1
                 end
             end
 
-            if @contain != 0
-                idf.value = Math.log(Float(@max)/Float(@contain))
+            if contain != 0
+                idf.value = Math.log(Float(max)/Float(contain))
             else
                 idf.value = 0
             end
@@ -42,8 +42,8 @@ class IdfsController < ApplicationController
     end
     
     def destroy
-        @idf = Idf.find_by(prop: params[:prop])
-        @idf.destroy
+        idf = Idf.find_by(prop: params[:prop])
+        idf.destroy
          redirect_to :back  #this is for temporary thing
 
     end
