1b849fdfa3693c499da2d8d1a30c9785957c040a
HurSungYeon <ethan0311@naver.com>
2015-05-24 16:19:08 +0900

    change tf and idf model file

--- app/models/tf.rb
+++ app/models/tf.rb
@@ -2,5 +2,5 @@ class Tf < ActiveRecord::Base
     belongs_to :hobby
 
     validates :value, :numericality => { :greater_than_or_equal_to => 1 }
-    validates :keyword, presence: true, uniqueness: true
+    validates :prop, presence: true, uniqueness: true
 end
