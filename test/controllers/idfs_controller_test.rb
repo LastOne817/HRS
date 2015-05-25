449ed8a830dc4ba28c4df5bca3a941bc1d8dba11
HurSungYeon <ethan0311@naver.com>
2015-05-25 21:07:35 +0900

    edit idf controller test

--- test/controllers/idfs_controller_test.rb
+++ test/controllers/idfs_controller_test.rb
@@ -3,23 +3,23 @@ class IdfsControllerTest < ActionController::TestCase
     test '#idf_createAll' do
         request.env["HTTP_REFERER"] = "/pages/main"
         post :createAll
-        t = Idf.find_by(:prop => "samplesample")
+        t = Idf.find_by(:prop => "collecting")
         assert t != nil
     end
 
     test '#idf_update' do
         request.env["HTTP_REFERER"] = "/pages/main"
         post :update
-        t = Idf.find_by(:prop => "sample")
-        assert (t.value - Math.log(2.5)).abs <= 0.01
+        t = Idf.find_by(:prop => "collecting")
+        assert t.value ==0
     end
 
 
     test '#idf_destroy' do
         request.env["HTTP_REFERER"] = "/pages/main"
         post :createAll
-        post :destroy, {"prop" => "sample"}
-        t = Idf.find_by(:prop => "sample")
+        post :destroy, {"prop" => "collecting"}
+        t = Idf.find_by(:prop => "collecting")
         assert t == nil
     end
 end
