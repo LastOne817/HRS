74715363cf900f898c76411641312e758cd9a774
LastOne817 <lastone817@naver.com>
2015-05-27 00:54:42 +0900

    Basic test for Hobby model

--- test/models/hobby_test.rb
+++ test/models/hobby_test.rb
@@ -1,7 +1,7 @@
 require 'test_helper'
 
 class HobbyTest < ActiveSupport::TestCase
-  # test "the truth" do
-  #   assert true
-  # end
+    test "#hobby_create" do
+        h = Hobby.create({:name => "Sample Hobby", :content => "This is a rich, active hobby"})
+    end
 end
