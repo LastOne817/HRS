0bbc0a84ecf97ff1f3bac1facaec8b6a275a905d
HurSungYeon <ethan0311@naver.com>
2015-05-26 23:21:35 +0900

    hotfix for hobby model

--- app/models/hobby.rb
+++ app/models/hobby.rb
@@ -2,4 +2,23 @@ class Hobby < ActiveRecord::Base
     has_many :tfs, :dependent => :destroy
 
     validates :name, presence: true
+
+    def self.create(name,text)
+        @hobby = Hobby.new(name: name)
+        @hobby.text = text
+        @hobby.save
+        @props = Property.all
+
+        @props.each do |prop|
+            Tf.create(prop,@hobby.id)
+        end
+
+        @tfs = hobby.tfs
+
+        @tfs.each do |tf|
+            Tf.update(@hobby.text,tf.id)
+        end
+
+        Idf.update
+    end
 end
