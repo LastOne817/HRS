71fc1d14a9910f4103ae5339308fcb959f991945
LastOne817 <lastone817@naver.com>
2015-05-27 03:02:10 +0900

    Created Hobby model test

--- test/models/hobby_test.rb
+++ test/models/hobby_test.rb
@@ -2,6 +2,18 @@ require 'test_helper'
 
 class HobbyTest < ActiveSupport::TestCase
     test "#hobby_create" do
-        h = Hobby.create({:name => "Sample Hobby", :content => "This is a rich, active hobby"})
+        p_rich = Property.find_by(:name => "rich")
+        p_rich.keywords.create(:name => "rich")
+        p_active = Property.find_by(:name => "active")
+        p_active.keywords.create(:name => "active")
+
+        h = Hobby.create({:name => "Sample Hobby", :content => "This is a rich active hobby"})
+
+        rich = h.tfs.find_by(prop: "rich")
+        refute_in_delta rich.value, 0
+        active = h.tfs.find_by(prop: "active")
+        refute_in_delta active.value, 0
+        inactive = h.tfs.find_by(prop: "inactive")
+        assert_in_delta inactive.value, 0
     end
 end
