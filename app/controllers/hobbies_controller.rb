8560f5ecf4def9dcbda43d2e6729467c1ee655dc
HurSungYeon <ethan0311@naver.com>
2015-05-31 15:07:16 +0900

    add controller of upload image of hobby

--- app/controllers/hobbies_controller.rb
+++ app/controllers/hobbies_controller.rb
@@ -19,8 +19,23 @@ class HobbiesController < ApplicationController
     def create
         hobby = Hobby.new(name: params[:hobby][:name])
         hobby.content = params[:hobby][:content]
-        if hobby.save
 
+        uploaded_io = params[:hobby][:photo]
+
+        if hobby.save
+            
+
+            if uploaded_io !=nil
+                File.open(Rails.root.join('public', 'hobbyimage', hobby.id.to_s), 'wb') do |file|
+                    file.write(uploaded_io.read)
+                end
+                hobby.image = "/hobbyimage/" + hobby.id.to_s
+                hobby.save
+            else
+                hobby.image = "http://placehold.it/900x300"
+                hobby.save
+            end
+              
             Tf.createAll(hobby.id)
 
             tfs = hobby.tfs
@@ -39,7 +54,20 @@ class HobbiesController < ApplicationController
         hobby = Hobby.find(params[:id])
         hobby.name = params[:hobby][:name]
         hobby.content = params[:hobby][:content]
+
+        uploaded_io = params[:hobby][:photo]
+
+
         if hobby.save
+
+            if uploaded_io !=nil
+                File.open(Rails.root.join('public', 'hobbyimage', hobby.id.to_s), 'wb') do |file|
+                    file.write(uploaded_io.read)
+                end
+                hobby.image = "/hobbyimage/" + hobby.id.to_s
+                hobby.save
+            end
+
             tfs = hobby.tfs
             content = hobby.content
 
